23e723ee765f067d84add74ebadcb2209a907be1
Yaroslav Panasenko <fenyochek@gmail.com>
2025-07-06 04:52:42 +0700

    fix(views): adjust get and post views, schema and model

--- src/contacts/schemas.py
+++ src/contacts/schemas.py
@@ -1,13 +1,18 @@
 from datetime import date
 from uuid import UUID
 
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr, Field, field_validator
 
 
 class ContactIn(BaseModel):
     name: str = Field(..., max_length=100)
     email: EmailStr
-    phone: str | None = Field(None, max_length=11)
+    phone: str | None = Field('', max_length=11)
+
+    @field_validator('phone', mode='before')
+    @classmethod
+    def set_empty_phone(cls, v):
+        return v or ''
 
     class Config:
         from_attributes = True
