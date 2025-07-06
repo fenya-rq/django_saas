2447181bcf688a459dbfbcb428ce5f1c2a23d937
Yaroslav Panasenko <fenyochek@gmail.com>
2025-07-07 04:19:08 +0700

    fix(contacts:views): adjust PATCH view; adjust views decorator, adjust updating schema

--- src/contacts/views.py
+++ src/contacts/views.py
@@ -4,19 +4,10 @@ from ninja import Router
 from ninja.pagination import PageNumberPagination, paginate
 
 from contacts.models import Contact
-from contacts.schemas import ContactIn, ContactOut
+from contacts.schemas import ContactIn, ContactOut, ContactUpdate
 from utilities.decorators import check_model_availability_for_tenant
 
 router = Router()
-
-
-@router.post('/contacts', response={201: ContactOut, 400: dict})
-def create_contact(request, payload: ContactIn):
-    try:
-        contact = Contact.objects.create(**payload.dict())
-        return 201, contact
-    except IntegrityError:
-        return 400, {'detail': 'Email already exists in this tenant.'}
 
 
 @router.get('/contacts', response=list[ContactOut])
@@ -28,21 +19,42 @@ def list_contacts(request):
 
 
 @router.get('/contacts/{contact_id}', response=ContactOut)
+@check_model_availability_for_tenant
 def get_contact(request, contact_id: str):
     contact = get_object_or_404(Contact, id=contact_id)
     return contact
 
 
+@router.post('/contacts', response={201: ContactOut, 400: dict})
+@check_model_availability_for_tenant
+def create_contact(request, payload: ContactIn):
+    try:
+        contact = Contact.objects.create(**payload.dict())
+        return 201, contact
+    except IntegrityError:
+        return 400, {'detail': 'Email already exists in this tenant.'}
+
+
 @router.put('/contacts/{contact_id}', response=ContactOut)
-def update_contact(request, contact_id: str, payload: ContactIn):
+@check_model_availability_for_tenant
+def update_contact(request, contact_id: str, payload: ContactUpdate):
     contact = get_object_or_404(Contact, id=contact_id)
-    for attr, value in payload.dict().items():
+
+    payload_dict = payload.dict(exclude_unset=True)
+
+    # Set existing email if not provided new one
+    if 'email' in payload_dict and payload_dict.get('email') is None:
+        payload_dict['email'] = contact.email
+
+    for attr, value in payload_dict.items():
         setattr(contact, attr, value)
+
     contact.save()
     return contact
 
 
 @router.delete('/contacts/{contact_id}', response={204: None})
+@check_model_availability_for_tenant
 def delete_contact(request, contact_id: str):
     contact = get_object_or_404(Contact, id=contact_id)
     contact.delete()
--- src/utilities/decorators.py
+++ src/utilities/decorators.py
@@ -7,11 +7,11 @@ from psycopg2.errors import UndefinedTable
 
 def check_model_availability_for_tenant(func):
     @wraps(func)
-    def inner(request):
+    def inner(request, *args, **kwargs):
         if request.tenant.schema_name == get_public_schema_name():
             raise HttpError(403, "Public Tenant can't access to shared apps model.")
         try:
-            result = func(request)
+            result = func(request, *args, **kwargs)
         except UndefinedTable:
             return HttpError(404, 'Required model does not exists')
         return result
