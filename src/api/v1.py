77deb99911c214c84cac2e804d2a44f8fbdec4b6
Yaroslav Panasenko <fenyochek@gmail.com>
2025-07-17 00:37:33 +0700

    tests: add new tests; adjust handling urls via reverse; adjust fixtures

--- src/api/v1.py
+++ src/api/v1.py
@@ -2,6 +2,6 @@ from ninja import NinjaAPI
 
 from contacts.views import router as contacts_router
 
-api_v1 = NinjaAPI()
+api_v1 = NinjaAPI(urls_namespace='api_v1')
 
 api_v1.add_router('v1/', contacts_router)
