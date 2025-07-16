14e9b7b400b497a0dbbf0110505e4f430453b7cb
Yaroslav Panasenko <fenyochek@gmail.com>
2025-07-16 16:35:29 +0700

    fix(contacts:views): adjust contact_id validation

--- src/contacts/views.py
+++ src/contacts/views.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from django.db import IntegrityError
 from django.shortcuts import get_object_or_404
 from ninja import Router
@@ -20,7 +22,7 @@ def list_contacts(request):
 
 @router.get('/contacts/{contact_id}', response=ContactOut)
 @check_model_availability_for_tenant
-def get_contact(request, contact_id: str):
+def get_contact(request, contact_id: UUID):
     contact = get_object_or_404(Contact, id=contact_id)
     return contact
 
@@ -37,12 +39,11 @@ def create_contact(request, payload: ContactIn):
 
 @router.put('/contacts/{contact_id}', response=ContactOut)
 @check_model_availability_for_tenant
-def update_contact(request, contact_id: str, payload: ContactUpdate):
+def update_contact(request, contact_id: UUID, payload: ContactUpdate):
     contact = get_object_or_404(Contact, id=contact_id)
 
     payload_dict = payload.dict(exclude_unset=True)
 
-    # Set existing email if not provided new one
     if 'email' in payload_dict and payload_dict.get('email') is None:
         payload_dict['email'] = contact.email
 
@@ -55,7 +56,7 @@ def update_contact(request, contact_id: str, payload: ContactUpdate):
 
 @router.delete('/contacts/{contact_id}', response={204: None})
 @check_model_availability_for_tenant
-def delete_contact(request, contact_id: str):
+def delete_contact(request, contact_id: UUID):
     contact = get_object_or_404(Contact, id=contact_id)
     contact.delete()
     return 204, None
